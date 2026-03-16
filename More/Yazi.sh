aa1e14b396234864c6bbd747278dce12ce72fba6
Nabil <Nabil@gmail.com>
2026-03-17 00:20:32 +0100

    yazi

--- More/Yazi.sh
+++ More/Yazi.sh
@@ -1,7 +1,30 @@
 # Yazi ====================================================================
 sudo pacman -S yazi
-cd -r Configs/yazi ~/.config
-# ya pkg add dangooddd/kanagawa
-# ya pkg add yazi-rs/plugins:full-border
+cp -r Configs/yazi ~/.config
 
+ya pkg add yazi-rs/plugins:full-border
 
+# Optional (but recommended)
+sudo pacman -S ffmpeg --noconfirm
+sudo pacman -S ffmpegthumbnailer --noconfirm
+sudo pacman -S 7zip --noconfirm
+sudo pacman -S jq --noconfirm
+sudo pacman -S poppler poppler-data --noconfirm
+sudo pacman -S fd --noconfirm
+sudo pacman -S ripgrep --noconfirm
+sudo pacman -S fzf --noconfirm
+sudo pacman -S zoxide --noconfirm
+sudo pacman -S resvg --noconfirm
+sudo pacman -S imagemagick --noconfirm
+sudo pacman -S xclip --noconfirm
+sudo pacman -S wl-clipboard --noconfirm
+sudo pacman -S xsel --noconfirm
+
+# NOTE:
+# ⚠️ Crucial Step: Activating Zoxide
+# Unlike the other tools, zoxide needs to be "hooked" into your shell to start learning your habits.
+#
+# If you are using Bash (edit ~/.bashrc):
+eval "$(zoxide init bash)"
+# If you are using Zsh (edit ~/.zshrc):
+eval "$(zoxide init zsh)"
