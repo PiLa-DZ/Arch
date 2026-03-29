cab044197cc8eedcf47c01d754a90a644ad5e6b4
Nabil <Nabil@gmail.com>
2026-03-29 17:44:56 +0100

    USP

--- How_To/Format_USB.sh
+++ How_To/Format_USB.sh
@@ -1,11 +1,10 @@
 lsblk
 sudo umount /dev/sdX1
-# Note: If mkfs.vfat or mkfs.exfat are missing, install them first: 
- 	sudo pacman -S dosfstools exfatprogs
+# Note: If mkfs.vfat or mkfs.exfat are missing, install them first:
+sudo pacman -S dosfstools exfatprogs
 
 # Target System,Format,Command
-sudo mkfs.vfat -F 32 -n "MY_USB" /dev/sdX1 # Windows/Mac/Linux,FAT32 
-sudo mkfs.exfat -L "MY_USB" /dev/sdX1 # Windows/Mac/Linux,exFAT 
-sudo mkfs.ext4 -L "MY_USB" /dev/sdX1 # Linux Only,EXT4 
-sudo mkfs.ntfs -L "MY_USB" /dev/sdX1 # Windows Only,NTFS 
-
+sudo mkfs.vfat -F 32 -n "MY_USB" /dev/sdb1 # Windows/Mac/Linux,FAT32
+sudo mkfs.ext4 -L "MY_USB" /dev/sdb1       # Linux Only,EXT4
+# sudo mkfs.exfat -L "MY_USB" /dev/sdb1 # Windows/Mac/Linux,exFAT
+# sudo mkfs.ntfs -L "MY_USB" /dev/sdb1 # Windows Only,NTFS
